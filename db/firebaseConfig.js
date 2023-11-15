45c85ee01021f40c7080382706543631df3d579a
AsepCodet <67466092+AsepCodet@users.noreply.github.com>
2023-11-16 05:29:34 +0800

    del-credential

--- db/firebaseConfig.js
+++ db/firebaseConfig.js
@@ -1,8 +1,6 @@
 /* firebaseConfig.js */
 import admin from 'firebase-admin';
-import serviceAccount from "../sdk/project-imam-37660-0cf411cd0948-SDK.json" assert { type: 'json' };
 
-const databaseURL = 'https://project-imam-37660-default-rtdb.asia-southeast1.firebasedatabase.app/'; // Replace with your Firebase database URL
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
