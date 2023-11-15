2534e1a975abfff8f593e77fbd96d30304cafc88
AsepCodet <67466092+AsepCodet@users.noreply.github.com>
2023-11-16 06:34:48 +0800

    modified:   .github/workflows/ci.yml 	modified:   db/firebaseConfig.js

--- db/firebaseConfig.js
+++ db/firebaseConfig.js
@@ -1,6 +1,8 @@
 /* firebaseConfig.js */
 import admin from 'firebase-admin';
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const databaseURL = process.env.DATABASE_URL
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
