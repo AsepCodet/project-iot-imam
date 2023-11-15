4d883cd457412ed8919528e9e4ee282b46d9c820
AsepCodet <67466092+AsepCodet@users.noreply.github.com>
2023-11-16 06:38:17 +0800

    modified:   db/firebaseConfig.js

--- db/firebaseConfig.js
+++ db/firebaseConfig.js
@@ -1,7 +1,7 @@
 /* firebaseConfig.js */
 import admin from 'firebase-admin';
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SECRETSERVICE);
 const databaseURL = process.env.DATABASE_URL
 
 admin.initializeApp({
